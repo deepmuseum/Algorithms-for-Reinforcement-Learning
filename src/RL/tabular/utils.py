59946b1f8c2cc0d8c1bd6d22e5fc52e822f22a99
deepmuseum <76712499+deepmuseum@users.noreply.github.com>
2021-01-13 22:40:22 +0100

    add eps greedy

--- src/RL/tabular/utils.py
+++ src/RL/tabular/utils.py
@@ -2,3 +2,9 @@ import numpy as np
 
 def bellman_operator(V, policy, R, P, gamma):
     raise NotImplementedError
+
+def eps_greedy(state, n_actions,Q, epsilon):
+  if np.random.uniform()<=epsilon:
+    return np.random.randint(0,n_actions)
+  else:
+    return np.argmax(Q[state])
