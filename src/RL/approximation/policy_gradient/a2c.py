7349aa552122e45675f2aa16b17e1a549224424f
CHAYBOUTI <chaybou@ca-sa.local>
2021-03-01 19:19:53 +0100

    true a2c

--- src/RL/approximation/policy_gradient/a2c.py
+++ src/RL/approximation/policy_gradient/a2c.py
@@ -48,18 +48,20 @@ class A2CAgent:
         optimizer_value,
         optimizer_actor,
         device,
-        obs_dim,
         n_a,
         path,
         test_every,
+        epochs,
+        batch_size,
     ):
 
+        self.epochs = epochs
+        self.batch_size = batch_size
         self.test_every = test_every
         self.path = path
         self.epsilon = -1
         self.env = env
         self.gamma = gamma
-        self.obs_dim = obs_dim
         self.n_a = n_a
         # Our two networks
         self.value_network = value_network.to(device)
@@ -73,39 +75,31 @@ class A2CAgent:
         self.best_average_reward = -float("inf")
 
     def _returns_advantages(self, rewards, dones, values, next_value):
-        """Returns the cumulative discounted rewards at each time step
-
-        Parameters
-        ----------
-        rewards : array
-            An array of shape (batch_size,) containing the rewards given by the env
-        dones : array
-            An array of shape (batch_size,) containing the done bool indicator given by the env
-        values : array
-            An array of shape (batch_size,) containing the values given by the value network
-        next_value : float
-            The value of the next state given by the value network
-
-        Returns
-        -------
-        returns : array
-            The cumulative discounted rewards
-        advantages : array
-            The advantages
         """
-        batch_size = len(rewards)
-        returns = np.zeros(batch_size)
-        for t in range(batch_size):
-            p = t
-            while p < batch_size and not dones[p]:
-                returns[t] += rewards[p] * self.gamma ** (p - t)
-                p = p + 1
-            if p == batch_size and not dones[-1]:
-                returns[t] += next_value * self.gamma ** (p - t)
-        advantages = returns - values
-        return returns, advantages
+        """
+        future_values = torch.empty(self.batch_size, device=self.device)
+        future_values[:-1] = values[1:]
+        future_values[-1] = next_value
+        # inefficient
+        # targets = torch.zeros(self.batch_size, device=self.device)
+        # i = 0
+        # while i < self.batch_size:
+        #     for j, (r, d) in enumerate(zip(rewards[i:], dones[i:])):
+        #         targets[i] += self.gamma ** (j - i) * r
+        #         if i + j == self.batch_size - 1 and not d:
+        #             targets[i] += self.gamma ** (j - i) * next_value
+        #         if d:
+        #             break
+        #     i += 1
+        #
+        # advantages = (targets - values).detach()
+        # efficient
+        targets = rewards + (1 - dones.to(torch.long)) * self.gamma * future_values
+        advantages = (targets - values).detach()
+        return targets, advantages
 
-    def select_from_prob(self, dist):
+    @staticmethod
+    def select_from_prob(dist):
         """
         Parameters
         ----------
@@ -117,76 +111,75 @@ class A2CAgent:
         action: int
             index of an action
         """
-        if np.random.uniform() >= self.epsilon:
-            action = int(torch.multinomial(dist, 1))
-        else:
-            action = np.random.choice(range(self.n_a))
+        action = int(torch.multinomial(dist, 1))
         return action
 
-    def training_batch(self, epochs, batch_size):
-        """Perform a training by batch
-
-        Parameters
-        ----------
-        epochs : int
-            Number of epochs
-        batch_size : int
-            The size of a batch
+    def train(self):
+        """
+        Perform a training by batch
         """
         episode_count = 0
-        actions = np.empty((batch_size,), dtype=np.int)
-        dones = np.empty((batch_size,), dtype=np.bool)
-        rewards, values = np.empty((2, batch_size), dtype=np.float)
-        observations = np.empty((batch_size,) + self.obs_dim, dtype=np.float)
+
         observation = self.env.reset()
         rewards_test = []
-        tk = tqdm(range(epochs))
+        tk = tqdm(range(self.epochs))
         for epoch in tk:
             # Lets collect one batch
-            for i in range(batch_size):
-                observations[i] = observation
+            actions = []
+            dones = []
+            rewards = []
+            observations = []
+            for i in range(self.batch_size):
                 observation = torch.tensor(
                     observation, dtype=torch.float, device=self.device
                 ).unsqueeze(0)
-                values[i] = self.value_network(observation)
-                self.epsilon = max(0.9 - epoch * 0.001, 0.0)
+                observations.append(observation)
                 dist_prob = self.actor_network(observation)
                 action = self.select_from_prob(dist_prob)
-                actions[i] = action
-                observation, rewards[i], dones[i], info = self.env.step(action)
-                if dones[i]:
+                actions.append(action)
+                observation, reward, done, info = self.env.step(action)
+                rewards.append(reward)
+                dones.append(done)
+                if dones[-1]:
                     observation = self.env.reset()
 
-            # If our epiosde didn't end on the last step we need to compute the value for the last state
+            # If our episode didn't end on the last step we need to compute the value for the last state
             if dones[-1]:
                 next_value = 0
+
             else:
                 next_value = self.value_network(
                     torch.tensor(
                         observation, dtype=torch.float, device=self.device
                     ).unsqueeze(0)
-                )
+                ).item()
 
             # Update episode_count
             episode_count += sum(dones)
+            observations = torch.cat(observations, dim=0)
+            values = self.value_network(observations).squeeze(-1)
 
             # Compute returns and advantages
-            returns, advantages = self._returns_advantages(
-                rewards, dones, values, next_value
+            targets, advantages = self._returns_advantages(
+                torch.tensor(rewards, device=self.device),
+                torch.tensor(dones, device=self.device),
+                values,
+                next_value,
             )
 
             # Learning step !
-            self.optimize_model(observations, actions, returns, advantages)
+            self.optimize_model(values, targets, advantages, actions, observations)
 
             # Test it every 50 epochs
-            if (epoch + 1) % self.test_every == 0 or epoch == epochs - 1:
+            if (epoch + 1) % self.test_every == 0 or epoch == self.epochs - 1:
                 rewards_test.append(np.array([self.evaluate() for _ in range(100)]))
                 mean_reward = round(rewards_test[-1].mean(), 2)
                 if mean_reward > self.best_average_reward:
                     self.best_average_reward = mean_reward
-                    self.best_state_dict = c(self.actor_network.cpu().state_dict())
-                    self.actor_network.to(self.device)
-                    pickle.dump(self.best_state_dict, open(self.path, "wb"))
+                    if self.path is not None:
+                        self.best_state_dict = c(self.actor_network.cpu().state_dict())
+                        self.actor_network.to(self.device)
+                        pickle.dump(self.best_state_dict, open(self.path, "wb"))
 
                 tk.set_postfix(
                     {
@@ -198,25 +191,17 @@ class A2CAgent:
 
         print(f"The trainnig was done over a total of {episode_count} episodes")
 
-    def optimize_model(self, observations, actions, returns, advantages):
-        returns = torch.tensor(returns[:, None], dtype=torch.float, device=self.device)
-        advantages = torch.tensor(advantages, dtype=torch.float, device=self.device)
-        observations = torch.tensor(observations, dtype=torch.float, device=self.device)
-        # MSE for the values
-        values = self.value_network(observations)
-        loss_value = F.mse_loss(values, returns)
+    def optimize_model(self, values, targets, advantages, actions, observations):
+        loss_value = F.mse_loss(values, targets)
         self.value_network_optimizer.zero_grad()
         loss_value.backward()
         # torch.nn.utils.clip_grad_norm_(self.value_network.parameters(), 1)
         self.value_network_optimizer.step()
-        # Actor & Entropy loss
-        loss_actor = 0
-        for t in range(len(observations)):
-            loss_actor -= (
-                torch.log(self.actor_network(observations[t].unsqueeze(0))[actions[t]])
-                * advantages[t]
-                / len(advantages)
-            )
+        probs = self.actor_network(observations).gather(
+            1,
+            torch.tensor(actions, device=self.device, dtype=torch.int64).unsqueeze(-1),
+        )
+        loss_actor = -torch.sum((torch.log(probs) * advantages / len(advantages)))
         self.actor_network_optimizer.zero_grad()
         loss_actor.backward()
         # torch.nn.utils.clip_grad_norm_(self.actor_network.parameters(), 1)
@@ -248,7 +233,6 @@ class A2CAgent:
 
 
 if __name__ == "__main__":
-
     from torch import optim
     import gym
 
@@ -274,12 +258,12 @@ if __name__ == "__main__":
     value_network_ = ValueNetwork(value_model)
     actor_network_ = ActorNetwork(actor_model)
 
-    gamma_ = 0.99
+    gamma_ = 0.9
 
-    value_network_optimizer = optim.RMSprop(value_network_.parameters(), lr=0.001)
-    actor_network_optimizer = optim.RMSprop(actor_network_.parameters(), lr=0.001)
+    value_network_optimizer = optim.RMSprop(value_network_.parameters(), lr=0.005)
+    actor_network_optimizer = optim.RMSprop(actor_network_.parameters(), lr=0.005)
 
-    device = torch.device("cuda")
+    device_ = torch.device("cpu")
     agent = A2CAgent(
         env=environment,
         actor_network=actor_network_,
@@ -287,8 +271,12 @@ if __name__ == "__main__":
         gamma=gamma_,
         optimizer_actor=actor_network_optimizer,
         optimizer_value=value_network_optimizer,
-        device=device,
+        device=device_,
         n_a=environment.action_space.n,
+        path=None,
+        test_every=100,
+        epochs=2000,
+        batch_size=32,
     )
 
-    agent.training_batch(1000, 256)
+    agent.train()
